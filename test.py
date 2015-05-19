764f3b03b02f52bd46bee5b160ea08977facf269
sysudengle <dengleshag@gmail.com>
2015-05-19 14:23:46 +0800

    v4

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import tornado.ioloop
 import tornado.web
+import tornado.autoreload
+import os
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -13,6 +15,10 @@ class ProductHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("./views/product.html")
 
+class NewsHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("./views/news.html")
+
 class FaqHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("./views/faq.html")
@@ -22,9 +28,23 @@ if __name__ == "__main__":
         (r"/", MainHandler),
         (r"/company", CompanyHandler),
         (r"/product", ProductHandler),
-        #(r"/news", MainHandler),
+        (r"/news", NewsHandler),
         (r"/faq", FaqHandler),
         (r"/images/(.*)", tornado.web.StaticFileHandler, {"path": "./images"},),
     ])
+
+    #application.add_handlers(r"^solbrighttech\.com$", [
+    #    (r"/", MainHandler),
+    #    (r"/company", CompanyHandler),
+    #    (r"/product", ProductHandler),
+    #    #(r"/news", MainHandler),
+    #    (r"/faq", FaqHandler),
+    #    (r"/images/(.*)", tornado.web.StaticFileHandler, {"path": "./images"},),
+    #])
     application.listen(8888)
+    tornado.autoreload.start()
+    for dir, _, files in os.walk('static'):
+        [tornado.autoreload.watch(dir + '/' + f) for f in files if not f.startswith('.')]
+    for dir, _, files in os.walk('static'):
+        [tornado.autoreload.watch(dir + '/views/' + f) for f in files if not f.startswith('.')]
     tornado.ioloop.IOLoop.instance().start()
