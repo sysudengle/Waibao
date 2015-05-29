be5783124042a100a0feb897b24a968ebd81c0e9
sysudengle <dengleshag@gmail.com>
2015-05-30 05:33:44 +0800

    dl

--- js/blockmove.js
+++ js/blockmove.js
@@ -4,10 +4,12 @@ $(function() {
 	$("#moving_block").css({'left' : (parseInt(nav_li.position().left)) + 'px', 'width' : nav_li.css("width")})
 	nav_font = $('#nl > a');
 	nav_font.css({'color' : 'white'});
-	$('.nav_container li a').mouseenter(function(){
+	$('.nav_container .nav_button a').mouseenter(function(){
 		var parent = $(this).parent();  
         left = parent.position().left;  
-		$('.nav_container li a').css({'color' : '#337ab7'});	
+		if(left >= $('#cal_li').position().left)
+			return;
+		$('.nav_container .nav_button a').css({'color' : '#337ab7'});	
         $("#moving_block").stop(true,true).animate({left:left, width: parent.css("width")}, "fast");  
 		$(this).css({'color' : 'white'});	
 	});	
